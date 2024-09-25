8a634166771608bc6af714846386e7594163bd67
shravan <skcode75@gmail.com>
2024-09-25 22:44:27 +0530

    Fix static files configuration for Render deployment

--- backend/build.sh
+++ backend/build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Create static directory if it doesn't exist
+mkdir -p static
+
 python manage.py collectstatic --no-input
 python manage.py migrate
